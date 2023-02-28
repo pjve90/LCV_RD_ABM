c49e255b427146afb38888939620263f1792d40e
Pablo Jose Varas Enriquez <pablo_varas@EVA.MPG.DE>
2023-02-28 15:48:37 +0100

    added some comments in habitat quality

--- Model code/habitat_quality.R
+++ Model code/habitat_quality.R
@@ -1,5 +1,7 @@
-# Habitat quality
+#' Habitat quality
+
+#'Habitat quality is understood as the amount of resources that an individual extracts from the environment. The amount is stage-specific because individuals can extract different amounts of resources from the environment depending on their life cycle stage.
 
 #stage-specific amount of resources that can be produced
-prod_n <- c(1,2,2,1) #juvenile, adult, reproductive career, post-reproductive
+habitat <- c(1,2,2,1) #juvenile, adult, reproductive career, post-reproductive
 
