a9b3cff32f4c7d071c63ac0d5ffa7c6bccc7087e
Pablo Jose Varas Enriquez <pablo_varas@EVA.MPG.DE>
2023-02-28 16:31:09 +0100

    trying the roxygen lingo

--- Model code/habitat_quality.R
+++ Model code/habitat_quality.R
@@ -1,7 +1,13 @@
 #' Habitat quality
+#'
+#' `habitat` returns the stage-specific amount of resources that an individual can produce from the environment.
+#'
+#' This is a vector containing the amount of resources that an individual can produces depending on her life cycle stage. The first value is for juvenile stage, the second one is for adult stage, the third one for reproductive career stage, and the fourth one for post-reproductive stage.
+#' 
+habitat <- c(1,2,2,1)
 
-#'Habitat quality is understood as the amount of resources that an individual extracts from the environment. The amount is stage-specific because individuals can extract different amounts of resources from the environment depending on their life cycle stage.
 
-#stage-specific amount of resources that can be produced
-habitat <- c(1,2,2,1) #juvenile, adult, reproductive career, post-reproductive
+
+
+
 
