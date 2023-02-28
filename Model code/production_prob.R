7d7a6c9c68216dacae2a4be44ee6111c778c4adc
Pablo Jose Varas Enriquez <pablo_varas@EVA.MPG.DE>
2023-02-28 16:41:24 +0100

    created the stage-specific production probabilities script

---
+++ Model code/production_prob.R
@@ -0,0 +1,7 @@
+#' Production probabilities
+#'
+#' `prod_prob` returns the stage-specific probabilities that an individual has to produce resources in that year.
+#'
+#' This is a vector containing the production probabilities of an individual depending on her life cycle stage. The first value is for juvenile stage, the second one is for adult stage, the third one for reproductive career stage, and the fourth one for post-reproductive stage.
+#' 
+prod_prob <- c(0.5,0.8,0.75,0.6)
