6ebe1a81ddc099fc1f73531adb04d138fdec1640
Pablo Jose Varas Enriquez <pablo_varas@EVA.MPG.DE>
2023-03-07 16:00:55 +0100

    almost all source links added

--- Model code/ABM_code_final.R
+++ Model code/ABM_code_final.R
@@ -3,52 +3,53 @@
 ## Resource production ----
 
 #Habitat quality
-source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/habitat_quality.R?token=GHSAT0AAAAAAB5C6IJP4LPU5FW5TFDKNVNUY77EEBQ")
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/habitat_quality.R?token=GHSAT0AAAAAAB5C6IJPFS4DAW6ELJXQMDPIZAHJ5HQ")
 
-#Production
-#stage-specific probabilities of production
-source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/production_prob.R?token=GHSAT0AAAAAAB5C6IJOW6AVVENY37LYDMQAY77EEGQ")
-#production function
-source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/production_fx.R?token=GHSAT0AAAAAAB5C6IJOSYCPF5WEIB7RJOZUY77EEMA")
-#production amount
-source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/production_amount.R?token=GHSAT0AAAAAAB5C6IJPPWOGQQYX7E5W6JK4Y77EEQQ")
+#Stage-specific probabilities of production
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/production_prob.R?token=GHSAT0AAAAAAB5C6IJOR7WHWDLGLPYCEOBUZAHJ5ZA")
+
+#Production function
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/production_fx.R?token=GHSAT0AAAAAAB5C6IJPTTBDHAP66WSMVNGQZAHJ5TA")
+
+#Production amount
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/production_amount.R?token=GHSAT0AAAAAAB5C6IJPHSD6FZ5T4LUBK5XSZAHJ5NA")
 
 ## Maternal investment ----
 
 #Identify if the mother has surplus of resources
-source()
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/mat_invest_mom_surp_identify.R?token=GHSAT0AAAAAAB5C6IJPMFWT5XPBUGSQMVM4ZAHJ7TA")
 
-#Identify the amount of surplus
-source()
+#Identify the amount of surplus of the mother
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/mat_invest_mom_surp_amount.R?token=GHSAT0AAAAAAB5C6IJPZIEAHCWDQ74N4HWKZAHJ7JQ")
 
 #Identify if the descendants need resources
-source()
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/mat_invest_desc_need_identify.R?token=GHSAT0AAAAAAB5C6IJPTCAB7PBQ7YGUZ6JOZAHKACQ")
 
 #Identify the amount of need for each descendant
-source()
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/mat_invest_desc_need_amount.R?token=GHSAT0AAAAAAB5C6IJPAD5AHTTHV544RUEKZAHKAUA")
 
 #Order the descendants by need and mother id
-source()
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/mat_invest_desc_order.R?token=GHSAT0AAAAAAB5C6IJOKHUDEMRGIGXPRBTKZAHJ66Q")
 
 #Mother invest in her descendants
-source()
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/mat_invest_fx.R?token=GHSAT0AAAAAAB5C6IJOKGTVL4OSATFFDYZUZAHJ7DA")
 
 ## Resource transfers ----
 
 # Specify the number of nodes and the number of blocks
-source()
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/transfers_param_sbm.R?token=GHSAT0AAAAAAB5C6IJOKC6F6S4DXMX62HKAZAHKBTA")
 
 #Define the block matrix
-source()
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/transfers_block_matrix.R?token=GHSAT0AAAAAAB5C6IJOYG3WSWIMRAKEEVWEZAHKCCA")
 
 #Define the surplus for transfers (max out degree in the network)
-source()
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/transfers_surplus_sbm.R?token=GHSAT0AAAAAAB5C6IJO3JYAV2AWHRAAUBMMZAHKDFQ")
 
 #Generate the network
-source()
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/transfers_sbm_fx.R?token=GHSAT0AAAAAAB5C6IJOPZBDSJDSP5OIFA4QZAHKDPQ")
 
 #Record the resources transferred
-source() 
+source("https://raw.githubusercontent.com/pjve90/LCV_RD_ABM/main/Model%20code/transfers_amount.R?token=GHSAT0AAAAAAB5C6IJP6VFYIFHPEAQM45F4ZAHKD4A") 
 
 ## Reproduction ----
 
@@ -70,16 +71,27 @@ source()
 #Lifetime reproductive output
 source()
 
-#Add newborns
+#Add newborns...not finished
 source()
 
 ## Transition ----
+
+#Time since last birth
+source()
 
 #Transition
 source()
 
 ## Survival ----
 
+#Survival cost
+source()
+
 #Survival
 source()
 
+#Discount of survival cost
+source()
+
+#Age
+source()
