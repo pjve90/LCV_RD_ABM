8ed96cfd76fbb99d4f17bdf8d19babbdb2d62d1b
dieterlukas <dieter.lukas@gmail.com>
2023-03-15 09:40:20 +0100

    fixed redundant variable name in mat_invest_fx

--- Model code/mat_invest_fx.R
+++ Model code/mat_invest_fx.R
@@ -10,8 +10,8 @@ mat_invest <- function(it_indpop){
     for(desc in 1:nrow(it_descpop_momsub)){   # Loop through all of the mother's descendants, starting with the offspring with the largest need
       if(it_indpop$mom_surplus_a[i] > it_descpop_momsub$desc_need_a[desc]) { # If the mother does not have sufficient resources to cover the needs of the current descendant, she does not invest
         it_indpop$mom_surplus_a[i] <- it_indpop$mom_surplus_a[i] - it_descpop_momsub$desc_need_a[desc] #discount the maternal investment for the mother
-        it_descpop_momsub$res_a[desc]  <- it_descpop_momsub$res_a[desc] + it_descpop_momsub$desc_need_a[desc] #add the maternal investment for the descendant
-        it_indpop$store_a[it_indpop$id == it_descpop_momsub$id[desc]] <- it_descpop_momsub$res_a[desc] #update the resources available of the descendant in the original database
+        it_descpop_momsub$store_a[desc]  <- it_descpop_momsub$store_a[desc] + it_descpop_momsub$desc_need_a[desc] #add the maternal investment for the descendant
+        it_indpop$store_a[it_indpop$id == it_descpop_momsub$id[desc]] <- it_descpop_momsub$store_a[desc] #update the resources available of the descendant in the original database
       }
     }
   }
