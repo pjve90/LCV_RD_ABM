62b0c4fa51952ce4aa711fd9402258d02cbda014
Dieter Lukas <37047199+dieterlukas@users.noreply.github.com>
2023-06-16 10:05:49 +0200

    Update mat_invest_desc_need_amount.R to deal with NA

--- Model_code/mat_invest_desc_need_amount.R
+++ Model_code/mat_invest_desc_need_amount.R
@@ -5,7 +5,7 @@
 #' This is a function that calculates the amount of resources that an individual needs from her mother to ensure her survival. It is based on the product from the subtraction between the survival cost (\code{surv_effort}) and the amount of resources stored the individual has (\code{store_a}), and if the individual needs resources or not (\code{desc_need}).
 #'
 desc_need_a <- function(it_indpop){
-  it_indpop$desc_need_a[i] <- (surv_cost - it_indpop$res_a[i])*it_indpop$desc_need[i]
-  return(it_indpop$desc_need_a)
+  if(is.na(it_indpop$desc_need[i])==F){it_indpop$desc_need_a[i] <- (surv_cost - it_indpop$res_a[i])*it_indpop$desc_need[i]
+  return(it_indpop$desc_need_a)}
 }
 
