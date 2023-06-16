1b226f0d3de0f5fa4e633fe4ef3ea14f725a0a9a
dieterlukas <dieter.lukas@gmail.com>
2023-06-16 16:59:52 +0200

    fixed maternal input function

--- Model_code/mat_invest_fx.R
+++ Model_code/mat_invest_fx.R
@@ -8,13 +8,15 @@ mat_invest <- function(it_indpop){
   if(it_mompop$mom_surplus_a[i] > 0){ #if the current individual is a mother of any descendants currently alive that need resources, and if she has a surplus of resources
       for(desc in 1:nrow(it_descpop_momsub)){   # Loop through all of the mother's descendants, starting with the offspring with the largest need
       if(it_mompop$mom_surplus_a[i] >= it_descpop_momsub$desc_need_a[desc]) { # If the mother has enough surplus to cover the need of her descendant, she invest
-        it_mompop$mom_surplus_a[i] <- it_mompop$mom_surplus_a[i] - it_descpop_momsub$desc_need_a[desc] #discount the maternal investment for the mother
+        it_mompop$mom_surplus_a[i] <- it_mompop$mom_surplus_a[i] - it_descpop_momsub$desc_need_a[desc]
+        it_indpop$res_a[it_indpop$id == it_mompop$id[i]] <- it_indpop$res_a[it_indpop$id == it_mompop$id[i]] - it_descpop_momsub$desc_need_a[desc] #discount the maternal investment for the mother
         it_descpop_momsub$res_a[desc]  <- it_descpop_momsub$res_a[desc] + it_descpop_momsub$desc_need_a[desc] #add the maternal investment for the descendant
-        it_descpop$res_a[it_descpop$id == it_descpop_momsub] <- it_descpop_momsub$res_a
+        
+        it_indpop$res_a[it_indpop$id == it_descpop_momsub$id[desc] ] <- it_indpop$res_a[it_indpop$id == it_descpop_momsub$id[desc] ] + it_descpop_momsub$desc_need_a[desc]
       }
     }
-    if(it_indpop$mom_surplus[it_mompop$id[i] %in% it_indpop$id]>0 ) {it_indpop$res_a[it_mompop$id[i] %in% it_indpop$id] <- it_mompop$mom_surplus_a[i]+surv_cost}
-    it_indpop$res_a[it_indpop$id[i] %in% it_descpop$id] <- it_descpop$res_a[i]
+ 
+   
   }
   
   return(it_indpop)
