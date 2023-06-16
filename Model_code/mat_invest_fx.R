0a7bd11c3363f863257e50c095bb6f0500a1c847
Dieter Lukas <37047199+dieterlukas@users.noreply.github.com>
2023-06-16 10:29:38 +0200

    Update mat_invest_fx.R to reflect the different subsets

--- Model_code/mat_invest_fx.R
+++ Model_code/mat_invest_fx.R
@@ -6,6 +6,7 @@
 
 mat_invest <- function(it_indpop){
   if(it_mompop$mom_surplus_a[i] > 0){ #if the current individual is a mother of any descendants currently alive that need resources, and if she has a surplus of resources
+    it_descpop_momsub<- as.data.frame(it_descpop[it_indpop$id[i] == it_descpop$mom_id, ]) #subset the descendants of this mother
     for(desc in 1:nrow(it_descpop_momsub)){   # Loop through all of the mother's descendants, starting with the offspring with the largest need
       if(it_mompop$mom_surplus_a[i] >= it_descpop_momsub$desc_need_a[desc]) { # If the mother has enough surplus to cover the need of her descendant, she invest
         it_mompop$mom_surplus_a[i] <- it_mompop$mom_surplus_a[i] - it_descpop_momsub$desc_need_a[desc] #discount the maternal investment for the mother
@@ -13,8 +14,8 @@ mat_invest <- function(it_indpop){
         it_descpop$res_a[it_descpop$id == it_descpop_momsub] <- it_descpop_momsub$res_a
       }
     }
-    it_indpop$res_a[it_mompop$id[i] %in% it_indpop$id] <- it_indpop$res_a[it_mompop$id[i] %in% it_indpop$id] - it_mompop$mom_surplus_a[i]
-    it_indpop$res_a[it_indpop$id[i] %in% it_descpop$id] <- it_indpop$res_a[it_indpop$id %in% it_descpop$id[i]] - it_descpop$res_a[i]
+    if(it_indpop$mom_surplus[it_mompop$id[i] %in% it_indpop$id]>0 ) {it_indpop$res_a[it_mompop$id[i] %in% it_indpop$id] <- it_mompop$mom_surplus_a[i]+surv_cost}
+    it_indpop$res_a[it_indpop$id[i] %in% it_descpop$id] <- it_descpop$res_a[i]
   }
   
   return(it_indpop)
