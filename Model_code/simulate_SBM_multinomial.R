52d27de53d1d68ebc9b0b417cb4e2d98051634c3
Daniel Redhead <44223661+danielRedhead@users.noreply.github.com>
2023-06-23 15:49:42 +0200

    created SN simulation using multinomial choice model

---
+++ Model_code/simulate_SBM_multinomial.R
@@ -0,0 +1,86 @@
+###########################################################################################################
+
+# Simulate network with SBM and node-specific max degree
+
+###########################################################################################################
+# Load softmax
+softmax <- function(par){
+  n.par <- length(par)
+  par1 <- sort(par, decreasing = TRUE)
+  Lk <- par1[1]
+  for (k in 1:(n.par-1)) {
+    Lk <- max(par1[k+1], Lk) + log1p(exp(-abs(par1[k+1] - Lk)))
+  }
+  val <- exp(par - Lk)
+  return(val)
+}
+
+create_self_noms <- function(N, probs) {
+  vectors <- vector("list", N)
+  for (i in 1:N) {
+    vectors[[i]] <- rep(0, N)
+    vectors[[i]][i] <- probs
+  }
+  return(vectors)
+}
+
+create_block_probs <- function(N, block_assignments) {
+  block_sizes <- table(block_assignments)
+  num_blocks <- length(block_sizes)
+  
+  vectors <- vector("list", N)
+  
+  for (i in 1:N) {
+    block <- block_assignments[i]
+    vector_length <- block_sizes[block]
+    
+    if (block == 1) {
+      vector <- c(rep(0.1, vector_length),
+                  rep(-1, vector_length),
+                  rep(-1, vector_length),
+                  rep(0, vector_length))
+    } else if (block == 2 || block == 3) {
+      vector <- c(rep(1, vector_length),
+                  rep(-1.2, vector_length),
+                  rep(-1.2, vector_length),
+                  rep(-0.01, vector_length))
+    } else if (block == 4) {
+      vector <- c(rep(1, vector_length),
+                  rep(-0.2, vector_length),
+                  rep(-0.2, vector_length),
+                  rep(-0.1, vector_length))
+    }
+    
+    vectors[[i]] <- vector
+  }
+  
+  vectors
+}
+# Load function
+simulate_SBM_multinomial <- function(N, max_deg, block_probs, self_noms) {
+		network <- matrix(0,N,N)	
+			for(i in 1:N){
+        		network[i, ] <- t(rmultinom(n = 1, size = max_deg[i], prob = softmax(self_noms[[i]] + block_probs[[i]])))
+              			}
+             kept_packets <- diag(network)
+             diag(network) <- 0 			
+             return(list(network = as.matrix(network), kept_packets = kept_packets))
+                }
+
+###########################################################################################################
+N <- 100
+food_returns <- c(rep(3, N/4), rep(5, N/4), rep(4, N/4), rep(3, N/4))
+block_assignments <- rep(1:4, each=N/4)
+simple_block_probs <- c(rep(0.1, N/4), rep(0.001, N/4), rep(0.001,N/4), rep(0.05, N/4))
+block_probs <- create_block_probs(N, block_assignments)
+
+self_noms <- create_vectors(N, 1)
+
+
+network <- simulate_SBM_multinomial(N, food_returns,block_probs, self_noms)
+g_net <- graph_from_adjacency_matrix(network$network)
+# Lets plot to see what the network looks like
+# We'll colour by age class
+library(igraph)
+plot(g_net, edge.arrow.size=0.15, vertex.size=5,
+      vertex.label = NA, vertex.color = block_assignments, edge.curved=0.4, layout = layout_nicely)
