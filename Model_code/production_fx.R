da69a124c300199dbe288ab3ca9167658eeba3fb
Pablo Jose Varas Enriquez <pablo_varas@EVA.MPG.DE>
2023-07-31 11:24:21 +0200

    updated description of the function

--- Model_code/production_fx.R
+++ Model_code/production_fx.R
@@ -1,8 +1,8 @@
-#' Resource production (probability outcome).
+#' Resource production.
 #'
-#' \code{produce} returns the output of the production probability of the individual.
+#' \code{produce} returns the amount of resources that an individual produces.
 #' 
-#' This is a function that defines if the individual produces resources that year (1) or not (0). It is based a Bernoulli distribution with \code{n} and \code{size} equal to 1, and \code{prob} based on the stage-specific values from \code{prod_prob}.
+#' This is a function that defines the amount of resources an individual produces. It is based a binomial distribution with \code{n} equal to 1, \code{size} defined as the stage-specific values from \code{maxprod}, and \code{prob} based on the stage-specific values from \code{prod_prob}.
 
 produce <- function(it_indpop){
   it_indpop$prod_a[i] <- rbinom(1, maxprod[it_indpop$stage[i]],prod_prob[it_indpop$stage[i]])
