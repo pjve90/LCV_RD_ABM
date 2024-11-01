36d797fcb6e65048fab294ff14fd5a9482a08ffb
Pablo Jose Varas Enriquez <pablo_varas@EVA.MPG.DE>
2024-11-01 17:49:56 +0100

    plot with median cv of all life history traits

--- Scenario_1/scenario_1_results.R
+++ Scenario_1/scenario_1_results.R
@@ -21,25 +21,6 @@ library(scales)
 #import raw results from the simulation
 results <- readRDS("./Scenario 1/lht_list_s1.RData")
 
-#Summary statistics ----
-
-summary_stats <- lapply(1:length(results),function(x)list())
-#match the original names with the sample
-names(summary_stats) <- names(results)
-names(summary_stats)
-
-#gather summary statistics of each result
-for (i in 1:length(results)){
-  if(nrow(results[[i]]) == 0){
-    summary_stats[[i]] <- summary_stats[[i]]
-  } else{
-    summary_stats[[i]]$summary <- summary(results[[i]][,c("lng","lro","asm","afr","alr","meno")])
-    summary_stats[[i]]$sd <- as.data.frame(lapply(results[[i]][,c("lng","lro","asm","afr","alr","meno")],sd,na.rm=T))
-  }
-}
-
-#Plot it! ----
-
 #get the parameter values for labeling
 
 #source the functions
@@ -56,6 +37,275 @@ maxprod <- max_production(habitat_quality)
 max_prod_prob <- seq(0.1,0.9,length=17)
 #stage-specific production probabilities.
 prod_prob <- production_prob(max_prod_prob)
+
+#Summary statistics ----
+
+summary_stats <- lapply(1:length(results),function(x)list())
+#match the original names with the sample
+names(summary_stats) <- names(results)
+names(summary_stats)
+
+#gather summary statistics of each result
+for (i in 1:length(results)){
+  if(nrow(results[[i]]) == 0){
+    summary_stats[[i]] <- summary_stats[[i]]
+  } else{
+    summary_stats[[i]]$summary <- summary(results[[i]][,c("lng","lro","asm","afr","alr","meno")])
+    summary_stats[[i]]$sd <- as.data.frame(lapply(results[[i]][,c("lng","lro","asm","afr","alr","meno")],sd,na.rm=T))
+    summary_stats[[i]]$mean <- as.data.frame(lapply(results[[i]][,c("lng","lro","asm","afr","alr","meno")],mean,na.rm=T))
+    summary_stats[[i]]$cv <- summary_stats[[i]]$sd / summary_stats[[i]]$mean  
+    if(summary_stats[[i]]$sd$lro == 0 & summary_stats[[i]]$mean$lro == 0){
+      summary_stats[[i]]$cv$lro <- 0
+    }
+  }
+}
+
+## CV sorted by life history trait ----
+
+#lng
+#create data frame to gather CVs of lng
+lng_cv <- as.data.frame(matrix(NA,17,11))
+colnames(lng_cv) <- c("p",1:10)
+lng_cv$p <- prod_prob[2,]
+#check it
+lng_cv
+#extract the CV of lng across the parameter space
+for(d in 1:nrow(lng_cv)){
+  for(r in 2:ncol(lng_cv)){
+    if(is.null(summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$lng) == T){
+      lng_cv[d,r] <- NA
+    }else{
+      lng_cv[d,r] <- summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$lng
+    }
+  }
+}
+#check it
+lng_cv
+
+#lro
+#create data frame to gather CVs of lro
+lro_cv <- as.data.frame(matrix(NA,17,11))
+colnames(lro_cv) <- c("p",1:10)
+lro_cv$p <- prod_prob[2,]
+#check it
+lro_cv
+#extract the CV of lro across the parameter space
+for(d in 1:nrow(lro_cv)){
+  for(r in 2:ncol(lro_cv)){
+    if(is.null(summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$lro) == T){
+      lro_cv[d,r] <- NA
+    }else{
+      lro_cv[d,r] <- summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$lro
+    }
+  }
+}
+#check it
+lro_cv
+
+#asm
+#create data frame to gather CVs of asm
+asm_cv <- as.data.frame(matrix(NA,17,11))
+colnames(asm_cv) <- c("p",1:10)
+asm_cv$p <- prod_prob[2,]
+#check it
+asm_cv
+#extract the CV of asm across the parameter space
+for(d in 1:nrow(asm_cv)){
+  for(r in 2:ncol(asm_cv)){
+    if(is.null(summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$asm) == T){
+      asm_cv[d,r] <- NA
+    }else{
+      asm_cv[d,r] <- summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$asm
+    }
+  }
+}
+#check it
+asm_cv
+
+#afr
+#create data frame to gather CVs of afr
+afr_cv <- as.data.frame(matrix(NA,17,11))
+colnames(afr_cv) <- c("p",1:10)
+afr_cv$p <- prod_prob[2,]
+#check it
+afr_cv
+#extract the CV of afr across the parameter space
+for(d in 1:nrow(afr_cv)){
+  for(r in 2:ncol(afr_cv)){
+    if(is.null(summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$afr) == T){
+      afr_cv[d,r] <- NA
+    }else{
+      afr_cv[d,r] <- summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$afr
+    }
+  }
+}
+#check it
+afr_cv
+
+#alr
+#create data frame to gather CVs of alr
+alr_cv <- as.data.frame(matrix(NA,17,11))
+colnames(alr_cv) <- c("p",1:10)
+alr_cv$p <- prod_prob[2,]
+#check it
+alr_cv
+#extract the CV of alr across the parameter space
+for(d in 1:nrow(alr_cv)){
+  for(r in 2:ncol(alr_cv)){
+    if(is.null(summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$alr) == T){
+      alr_cv[d,r] <- NA
+    }else{
+      alr_cv[d,r] <- summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$alr
+    }
+  }
+}
+#check it
+alr_cv
+
+#meno
+#create data frame to gather CVs of meno
+meno_cv <- as.data.frame(matrix(NA,17,11))
+colnames(meno_cv) <- c("p",1:10)
+meno_cv$p <- prod_prob[2,]
+#check it
+meno_cv
+#extract the CV of meno across the parameter space
+for(d in 1:nrow(meno_cv)){
+  for(r in 2:ncol(meno_cv)){
+    if(is.null(summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$meno) == T){
+      meno_cv[d,r] <- NA
+    }else{
+      meno_cv[d,r] <- summary_stats[grep(paste("d",d,"",sep="_"),names(summary_stats))][[r-1]]$cv$meno
+    }
+  }
+}
+#check it
+meno_cv
+
+## Median CV per life history trait ----
+
+#Longevity
+#create data frame
+lht_median <- as.data.frame(matrix(NA,ncol(prod_prob),7))
+colnames(lht_median) <- c("p","lng","lro","asm","afr","alr","meno")
+lht_median$p <- prod_prob[2,]
+lht_median
+#calculate the median CV per life history trait
+for(i in 1:nrow(lht_median)){
+  lht_median[i,"lng"] <- ifelse(sum(is.na(lng_cv[i,2:ncol(lng_cv)]))==10,NA,median(as.numeric(lng_cv[i,2:ncol(lng_cv)]),na.rm=T))
+  lht_median[i,"lro"] <- ifelse(sum(is.na(lro_cv[i,2:ncol(lro_cv)]))==10,NA,median(as.numeric(lro_cv[i,2:ncol(lro_cv)]),na.rm=T))
+  lht_median[i,"asm"] <- ifelse(sum(is.na(asm_cv[i,2:ncol(asm_cv)]))==10,NA,median(as.numeric(asm_cv[i,2:ncol(asm_cv)]),na.rm=T))
+  lht_median[i,"afr"] <- ifelse(sum(is.na(afr_cv[i,2:ncol(afr_cv)]))==10,NA,median(as.numeric(afr_cv[i,2:ncol(afr_cv)]),na.rm=T))
+  lht_median[i,"alr"] <- ifelse(sum(is.na(alr_cv[i,2:ncol(alr_cv)]))==10,NA,median(as.numeric(alr_cv[i,2:ncol(alr_cv)]),na.rm=T))
+  lht_median[i,"meno"] <- ifelse(sum(is.na(meno_cv[i,2:ncol(meno_cv)]))==10,NA,median(as.numeric(meno_cv[i,2:ncol(meno_cv)]),na.rm=T))
+}
+lht_median
+
+#Plot it! ----
+
+#get the maximum CV of all the life history traits for the scale in the plots
+#create empty vector
+cv_all <- NA
+#select the CV of all life history traits
+for(i in 1:length(summary_stats)){
+  if(is.null(summary_stats[[i]])==T){
+    cv_all <- c(cv_all,NA)
+  }else{
+    cv_all <- c(cv_all,summary_stats[[i]]$cv)
+  }
+  cv_all <- as.numeric(cv_all)
+  cv_all <- cv_all[!is.na(as.numeric(cv_all))]
+}
+#get the maximum
+max(cv_all)
+
+#plot the CV together
+par(mfrow=c(1,6))
+layout(matrix(c(1,1,2,2,3,4,5,6),ncol=4,byrow=T))
+#longevity
+plot(c(0,max(cv_all)+0.5)~c(0,1),
+     main="Longevity",
+     xlab="Parameter value",
+     ylab="CV longevity",
+     ylim=c(0,max(cv_all)),
+     type="n")
+for(d in 1:nrow(lng_cv)){
+    points(as.numeric(lng_cv[d,2:11])~rep(lng_cv[d,1],10),
+           pch=16,
+           col=alpha("grey",0.5)
+           )    
+}
+points(lht_median$lng~lht_median$p,pch=16,cex=1)
+#lro
+plot(c(0,max(cv_all)+0.5)~c(0,1),
+     main="Lifetime\nreproductive output",
+     xlab="Parameter value",
+     ylab="CV lifetime reproductive output",
+     ylim=c(0,max(cv_all)),
+     type="n")
+for(d in 1:nrow(lro_cv)){
+  points(as.numeric(lro_cv[d,2:11])~rep(lro_cv[d,1],10),
+         pch=16,
+         col=alpha("grey",0.5)
+  )    
+}
+points(lht_median$lro~lht_median$p,pch=16,cex=1)
+#asm
+plot(c(0,max(cv_all)+0.5)~c(0,1),
+     main="Age at\nsexual maturity",
+     xlab="Parameter value",
+     ylab="CV age at sexual maturity",
+     ylim=c(0,max(cv_all)),
+     type="n")
+for(d in 1:nrow(asm_cv)){
+  points(as.numeric(asm_cv[d,2:11])~rep(asm_cv[d,1],10),
+         pch=16,
+         col=alpha("grey",0.5)
+  )    
+}
+points(lht_median$asm~lht_median$p,pch=16,cex=1)
+#afr
+plot(c(0,max(cv_all)+0.5)~c(0,1),
+     main="Age at\nfirst reproduction",
+     xlab="Parameter value",
+     ylab="CV age at first reproduction",
+     ylim=c(0,max(cv_all)),
+     type="n")
+for(d in 1:nrow(afr_cv)){
+  points(as.numeric(afr_cv[d,2:11])~rep(afr_cv[d,1],10),
+         pch=16,
+         col=alpha("grey",0.5)
+  )    
+}
+points(lht_median$afr~lht_median$p,pch=16,cex=1)
+#alr
+plot(c(0,max(cv_all)+0.5)~c(0,1),
+     main="Age at\nlast reproduction",
+     xlab="Parameter value",
+     ylab="CV age at last reproduction",
+     ylim=c(0,max(cv_all)),
+     type="n")
+for(d in 1:nrow(alr_cv)){
+  points(as.numeric(alr_cv[d,2:11])~rep(alr_cv[d,1],10),
+         pch=16,
+         col=alpha("grey",0.5)
+  )    
+}
+points(lht_median$alr~lht_median$p,pch=16,cex=1)
+#meno
+plot(c(0,max(cv_all)+0.5)~c(0,1),
+     main="Age at\nmenopause",
+     xlab="Parameter value",
+     ylab="CV age at menopause",
+     ylim=c(0,max(cv_all)),
+     type="n")
+for(d in 1:nrow(meno_cv)){
+  points(as.numeric(meno_cv[d,2:11])~rep(meno_cv[d,1],10),
+         pch=16,
+         col=alpha("grey",0.5)
+  )    
+}
+points(lht_median$meno~lht_median$p,pch=16,cex=1)
 
 #Each parameter value separately ----
 
