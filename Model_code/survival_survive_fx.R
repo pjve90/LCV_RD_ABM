b58fdd77a7eedac49504c08c8c5441d1d6e9da02
Pablo Jose Varas Enriquez <pablo_varas@EVA.MPG.DE>
2024-11-22 11:50:53 +0100

    found that there was no 100 years limit for survival function

--- Model_code/survival_survive_fx.R
+++ Model_code/survival_survive_fx.R
@@ -5,7 +5,7 @@
 #' This is a function that defines whether an individual survives or not, depending on the amount of resources stored by the individual. If the individual has more resources than the survival cost then she will survive (1), if she is below that amount then she will die (0).
 
 survive <- function(it_indpop){
-  if(it_indpop$res_a[i] >= surv_cost){
+  if(it_indpop$res_a[i] >= surv_cost & it_indpop$age[i]<= 100){
       it_indpop$surv[i] <- 1
     } else{
       it_indpop$surv[i] <- 0
